8922534bbaa9d0a25329b528e27f503984393e60
Rhyannon Joy Rodriguez <rhyannonjoy@gmail.com>
2025-12-16 15:23:28 -0800

    docs(intro): draft introduction content

--- docs/intro.md
+++
@@ -1,4 +0,0 @@
-# Introduction
-
-_coming soon_
-<!-- this should be like a table of contents / index / home? -->
--- sidebars.ts
+++ sidebars.ts
@@ -19,7 +19,7 @@ const sidebars: SidebarsConfig = {
   // But you can create a sidebar manually
   /*
   tutorialSidebar: [
-    'intro',
+    'introduction',
     'hello',
     {
       type: 'category',
